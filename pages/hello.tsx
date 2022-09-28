e2a4dc878b17cbd27eb603510cdb95a175764570
DheerajBisht <dheerajbisht362@gmail.com>
2022-09-28 22:16:25 +0530

    header-component

---
+++ pages/hello.tsx
@@ -0,0 +1,12 @@
+import HeaderComponent from "../components/common/header";
+import FooterComponent from "../components/common/footer";
+
+function HelloPage() {
+  return (
+    <div>
+      <HeaderComponent></HeaderComponent>
+      <FooterComponent></FooterComponent>
+    </div>
+  );
+}
+export default HelloPage;
